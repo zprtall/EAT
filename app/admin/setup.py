5158d80c18139f9ae8371559f5c0cacd2d63fd21
zprtal <tuzgleb75@gmail.com>
2026-07-07 13:45:25 +0300

    fix admin auth

--- app/admin/setup.py
+++ app/admin/setup.py
@@ -1,5 +1,5 @@
 from sqladmin import Admin
-
+from app.admin.auth import AdminAuth
 from app.admin.views import (
     Users,
 
@@ -10,7 +10,10 @@ def setup_admin(app, engine):
 
     admin = Admin(
         app,
-        engine
+        engine,
+        authentication_backend=AdminAuth(
+            secret_key="SUPER_SECRET"
+        )
     )
 
     admin.add_view(Users)
