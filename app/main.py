01e0da8f866d884cc0448e79f7c36e58bf2b4e2f
zprtal <tuzgleb75@gmail.com>
2026-07-09 10:35:23 +0300

    fix(admin): all models + user and global settings

--- app/main.py
+++ app/main.py
@@ -12,6 +12,9 @@ from app.api.library import router as library_router
 from app.api.product import router as product_router
 from app.api.target import router as target_router
 
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
+
+
 
 app = FastAPI(title="app", version="1.0.0")
 app.include_router(meal_router)
@@ -20,9 +23,10 @@ app.include_router(dish_router)
 app.include_router(library_router)
 app.include_router(product_router)
 app.include_router(target_router)
-
 app.add_middleware(
     SessionMiddleware,
+    ProxyHeadersMiddleware,
+    allowed_hosts=["eat.prosandbox.ru"],
     secret_key="SUPER_SECRET"
 )
 
